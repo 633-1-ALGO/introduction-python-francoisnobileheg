536f9293205440fe18d970a6daabdaadaa176cd1
francoisnobileheg <francois.nobile@etu.hesge.ch>
2019-09-16 09:49:52 +0200

    exercices

--- 9.1- Exercice - Traitement de string/string1.py
+++ 9.1- Exercice - Traitement de string/string1.py
@@ -1,4 +1,4 @@
 # Consigne : Récupérer le mot "chaine" du string s et l'afficher
 s = 'un exemple de chaine'
 
-print(s[14:20])
+print(s[14:20]) 
--- 9.2  Exercice - Traitement de string/string2.py
+++ 9.2  Exercice - Traitement de string/string2.py
@@ -1,3 +1,13 @@
 # Consigne : Rechercher le nombre d'occurences du mot "exemple" et l'afficher. Remplacer le mot "est" par "représente".
 # Bonus : Inverser le sens de lecture.
 texte = "Ceci est un exemple exemplaire d'exemple exempté d'exemple."
+
+textsplit = texte.split()
+
+nb = 0
+
+for i in textsplit:
+    if i.find("exemple"):
+        nb += 1
+
+print("Nombres d'occurence du mot exemple : ", nb)
