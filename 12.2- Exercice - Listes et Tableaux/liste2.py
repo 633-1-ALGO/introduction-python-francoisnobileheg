8282a837f2bed7ff17ee66916847b7aa4f6cd833
francoisnobileheg <francois.nobile@etu.hesge.ch>
2019-09-22 09:19:42 +0200

    exercices 12.2 13.1

--- 12.2- Exercice - Listes et Tableaux/liste2.py
+++ 12.2- Exercice - Listes et Tableaux/liste2.py
@@ -2,3 +2,19 @@
 # Résultat attendu : Un affichage comme ceci : "La valeur maximum est :  x  et elle se trouve à l'indice [ n ][ m ]
 
 tab = [[4, 7, 3, 20, 42], [2, 4, 5, 7, 2], [23, 24, 15, 75, 23]]
+
+maximum = 0
+pos_i = 0
+pos_j = 0
+
+for i in range(0, len(tab)):
+    for j in range(0, len(tab[i])):
+        print(tab[i][j])
+        if tab[i][j] > maximum:
+            maximum = tab[i][j]
+            pos_i = i
+            pos_j = j
+
+sortie = "La valeur maximum est : {} et elle se trouve à l'indice [{}][{}]".format(maximum, pos_i, pos_j)
+
+print(sortie)
--- 13.1- Exercice - Fonctions/fonction.py
+++ 13.1- Exercice - Fonctions/fonction.py
@@ -4,3 +4,14 @@ texte = "ceci est un texte que vous pouvez modifier mais gare aux caracteres spe
 tab_lettres = [
     ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
      'x', 'y', 'z', ' '], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+cpt = 0
+
+for i in range(0, len(tab_lettres[0])):
+    for l in texte:
+        if l == tab_lettres[0][i]:
+            cpt += 1
+    tab_lettres[1][i] = cpt
+    cpt = 0
+
+print(tab_lettres[1])
